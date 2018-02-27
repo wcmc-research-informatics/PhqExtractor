cc760fe702d5c26aeeebe3b6c4aef5308786ec47
Prakash Adekkanattu <pra2008@med.cornell.edu>
2018-02-27 11:25:09 -0500

    Added target folder

---
+++ target/classes/listeners/SQLDatabaseListenerConfig.groovy
@@ -0,0 +1,42 @@
+package listeners
+
+import gov.va.vinci.ef.listeners.BasicDatabaseListener
+
+int batchSize = 1000
+
+String url = "jdbc:sqlserver://vits-archsqlp02.med.cornell.edu;databasename=DM_NLP;integratedSecurity=false"
+String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+String dbUser = "usr_dm_nlp"
+String dbPwd = "us3r_83_Nlp"
+String dbsName = "DM_NLP"
+String tableName = "dbo.DEPRESSION_PHQ_EVAL_LEO_OUTPUT"
+
+incomingTypes = "gov.va.vinci.ef.types.Relation"
+// incomingTypes = "gov.va.vinci.ef.types.ContextWindow"
+
+fieldList = [
+        ["DocId", "0", "varchar(500)"],
+        ["Term", "-1", "varchar(500)"],
+        ["Value", "-1", "varchar(100)"],
+        ["Value2", "-1", "varchar(100)"],
+        ["ValueString", "-1", "varchar(100)"],
+        ["InstanceID", "-1", "int"],
+        ["Snippets", "-1", "varchar(8000)"],
+        ["SpanStart", "-1", "int"],
+        ["SpanEnd", "-1", "int"]
+]
+
+boolean dropExisting = false;
+listener = BasicDatabaseListener.createNewListener(
+        driver,
+        url,
+        dbUser,
+        dbPwd,
+        dbsName,
+        tableName,
+        batchSize,
+        fieldList,
+        incomingTypes)
+
+// Comment out the statement below if you want to add to the existing table
+listener.createTable(dropExisting);
---
+++ target/classes/readers/MySQLDatabaseCollectionReaderConfig.groovy
@@ -0,0 +1,30 @@
+package readers
+
+import gov.va.vinci.leo.cr.BatchDatabaseCollectionReader;
+
+String driver = "com.mysql.jdbc.Driver"
+String url = "jdbc:mysql://localhost:3306/psychiatric?autoReconnect=true&useSSL=false"
+String username="psych_user";
+String password="psych_user";
+
+// String query = "SELECT ID, NOTE_TEXT FROM psychiatric.depression_phq_eval"
+// String query = "SELECT ID, NOTE_TEXT FROM psychiatric.depression_phq_eval where LeoStatus= 'FP'"
+// String query = "SELECT ID, NOTE_TEXT FROM psychiatric.depression_phq_eval where ID = 140 "
+// String query = "SELECT ID, NOTE_TEXT FROM psychiatric.test_notes where id in (6)"
+// String query = "SELECT ID, NOTE_TEXT FROM psychiatric.test_notes"
+
+String query = "SELECT ID, NOTE_TEXT FROM psychiatric.phq_test"
+
+int startingIndex = 0;
+int endingIndex = 10;
+int batch_size = 10000;
+
+reader = new BatchDatabaseCollectionReader(
+        driver,
+        url,
+        username,
+        password,
+        query,
+        "id","note_text",   /// Make sure that the names of the fields are low case.
+        startingIndex , endingIndex
+        , batch_size)
