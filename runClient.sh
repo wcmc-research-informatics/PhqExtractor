c33ba7b780e5846d9ba4c5f44cbf1be751bf8873
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-20 09:19:59 +0800

    Delete runClient.sh

--- runClient.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-##java -Xmx1024m -Xms256m -Dlog4j.configuration=file:config/log4j.properties -cp "lib/*:target/*" gov.va.vinci.ef.Client -clientConfigFile "config/ClientConfig.groovy" -readerConfigFile "config/readers/KnowtatorCollectionReaderConfig.groovy" -listenerConfigFile "config/listeners/CsvListenerConfig.groovy" -listenerConfigFile="config/listeners/AuCompareSummaryListenerConfig.groovy"  -listenerConfigFile "config/listeners/"
-
-java -Xmx2048m -Xms512m -Dlog4j.configuration=file:config/log4j.properties -cp "lib/*:target/*" gov.va.vinci.ef.Client -clientConfigFile "config/ClientConfig.groovy" -readerConfigFile "config/readers/MySQLDatabaseCollectionReaderConfig.groovy" -listenerConfigFile "config/listeners/MySQLDatabaseListenerConfig.groovy"
