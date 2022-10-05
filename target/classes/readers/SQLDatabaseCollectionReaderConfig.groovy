2e12b36c5f925871795a97a08df93832276ddf22
pra2008 <pra2008@med.cornell.edu>
2022-10-05 09:34:31 -0400

    Update SQLDatabaseCollectionReaderConfig.groovy
    updated to remove authentication details

--- target/classes/readers/SQLDatabaseCollectionReaderConfig.groovy
+++ target/classes/readers/SQLDatabaseCollectionReaderConfig.groovy
@@ -3,9 +3,9 @@ package readers
 import gov.va.vinci.leo.cr.BatchDatabaseCollectionReader;
 
 String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-String url = "jdbc:sqlserver://vits-archsqlp02.med.cornell.edu;databasename=DM_NLP;integratedSecurity=false"
-String username="usr_dm_nlp";
-String password="us3r_83_Nlp";
+String url = "jdbc:sqlserver://<server>;databasename=DM_NLP;integratedSecurity=false"
+String username="<user>";
+String password="<paswd>";
 String query = "SELECT ID, NOTE_TEXT FROM (SELECT ROW_NUMBER() OVER (ORDER BY (Select 1)) AS rowNum, CONCAT(PAT_ID, '|', PAT_ENC_CSN_ID, '|', NOTE_ID, '|', NOTE_CSN_ID, '|', NOTE_TYPE, '|', CONVERT(VARCHAR, CONTACT_DATE,121) ) AS ID, NOTE_TEXT FROM DM_NLP.dbo.DEPRESSION_PHQ_EVAL ) tab WHERE rowNum between 0 and 1001"
 
 // String query = "SELECT CONCAT(PAT_ID, '|', PAT_ENC_CSN_ID, '|', NOTE_ID, '|', NOTE_CSN_ID, '|', NOTE_TYPE, '|', CONVERT(VARCHAR, CONTACT_DATE, 121) ) AS ID, NOTE_TEXT FROM DM_NLP.dbo.depression_phq_notes"
