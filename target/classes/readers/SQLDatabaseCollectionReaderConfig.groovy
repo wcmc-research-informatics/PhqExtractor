456b1f7c3282900426280a40814bef6daecfb8a6
pra2008 <pra2008@med.cornell.edu>
2022-10-05 09:47:20 -0400

    Update SQLDatabaseCollectionReaderConfig.groovy

--- target/classes/readers/SQLDatabaseCollectionReaderConfig.groovy
+++ target/classes/readers/SQLDatabaseCollectionReaderConfig.groovy
@@ -6,10 +6,8 @@ String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
 String url = "jdbc:sqlserver://<server>;databasename=DM_NLP;integratedSecurity=false"
 String username="<user>";
 String password="<paswd>";
-String query = "SELECT ID, NOTE_TEXT FROM (SELECT ROW_NUMBER() OVER (ORDER BY (Select 1)) AS rowNum, CONCAT(PAT_ID, '|', PAT_ENC_CSN_ID, '|', NOTE_ID, '|', NOTE_CSN_ID, '|', NOTE_TYPE, '|', CONVERT(VARCHAR, CONTACT_DATE,121) ) AS ID, NOTE_TEXT FROM DM_NLP.dbo.DEPRESSION_PHQ_EVAL ) tab WHERE rowNum between 0 and 1001"
+String query = "<query>"
 
-// String query = "SELECT CONCAT(PAT_ID, '|', PAT_ENC_CSN_ID, '|', NOTE_ID, '|', NOTE_CSN_ID, '|', NOTE_TYPE, '|', CONVERT(VARCHAR, CONTACT_DATE, 121) ) AS ID, NOTE_TEXT FROM DM_NLP.dbo.depression_phq_notes"
-// String query = "SELECT  ID, NOTE_TEXT FROM DM_NLP.dbo.PHQ_NOTES where RowNo > 0 and RowNo < 100"
 
 int startingIndex = 0;
 int endingIndex = 10;
